44491b430b1a99942a32dbdaff7b863817470eee
Hannah Olbrich <hannaholbrich@gmail.com>
2026-01-07 23:04:05 +0000

    re-add deploeyd site name to vite config

--- music-theory-app/src/App.jsx
+++ music-theory-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/music-theory-app">
       <Navbar />
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
