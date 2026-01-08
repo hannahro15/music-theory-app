828ab19db70560d6e7e21fedad72088119fe5b71
Hannah Olbrich <hannaholbrich@gmail.com>
2026-01-08 10:50:39 +0000

    Update routing stucture with Navlink and browserrouter in main.jsx

--- music-theory-app/src/App.jsx
+++ music-theory-app/src/App.jsx
@@ -1,20 +1,18 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Scales from './pages/scales/scales.jsx'
 
 function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/scales" element={<Scales />} />
         <Route path="/ear-training" element={<h1>Ear Training</h1>} />
       </Routes>
-    </BrowserRouter>
     </div>
   )
 }
--- music-theory-app/src/components/Navbar.jsx
+++ music-theory-app/src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Navbar as RBNavbar, Nav, NavDropdown, Container, Dropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
     <RBNavbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <RBNavbar.Brand as={Link} to="/">Music Theory</RBNavbar.Brand>
+        <RBNavbar.Brand as={NavLink} to="/">Music Theory</RBNavbar.Brand>
         <RBNavbar.Toggle aria-controls="main-nav" />
         <RBNavbar.Collapse id="main-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
 
             <NavDropdown title="Theory" id="theory-dropdown">
-              <NavDropdown.Item as={Link} to="/scales">Scales</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/scales">Scales</NavDropdown.Item>
               <NavDropdown.Item href="#arpeggios">Arpeggios</NavDropdown.Item>
               <NavDropdown.Item href="#dom-dim">Dominant and Diminished 7ths</NavDropdown.Item>
               <NavDropdown.Item href="#chord-construction">Chord Construction</NavDropdown.Item>
@@ -24,7 +24,7 @@ function Navbar() {
             </NavDropdown>
                 
             <NavDropdown title="Activities" id="quizzes-dropdown">
-              <NavDropdown.Item as={Link} to="/ear-training">Ear training</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/ear-training">Ear training</NavDropdown.Item>
               <NavDropdown.Item href="#notation">Notation</NavDropdown.Item>
               <NavDropdown.Item href="#intervals">Intervals</NavDropdown.Item>
               <NavDropdown.Item href="#chords">Chords</NavDropdown.Item>
