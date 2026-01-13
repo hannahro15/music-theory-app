a0377d446e59b4b74b02dfc4140c133191cb3573
Hannah Olbrich <hannaholbrich@gmail.com>
2026-01-13 13:11:39 +0000

    Add placeholders for 4th and 5ths intervals

--- music-theory-app/src/pages/intervals/Intervals.jsx
+++ music-theory-app/src/pages/intervals/Intervals.jsx
@@ -1,11 +1,13 @@
-import Min2nd from './min2nd.jsx';
-import Maj2nd from './maj2nd.jsx';
-import Min3rd from './min3rd.jsx';
-import Maj3rd from './maj3rd.jsx';
+import Min2nd from './Min2nd.jsx';
+import Maj2nd from './Maj2nd.jsx';
+import Min3rd from './Min3rd.jsx';
+import Maj3rd from './Maj3rd.jsx';
+import Perfect4th from './Perfect4th.jsx';
+import Tritone from './Tritone.jsx';
+import Perfect5th from './Perfect5th.jsx';
 
 function Intervals() {
   return (
-
     <div>
       <h1>Intervals Page</h1>
       <p>This is the Intervals page content.</p>
@@ -13,9 +15,9 @@ function Intervals() {
       <Maj2nd />
       <Min3rd />
       <Maj3rd />
-      <div>Perfect 4th</div>
-      <div>Augmented 4th / Diminished 5th (Also known as a tritone)</div>
-      <div>Perfect 5th</div>
+      <Perfect4th />
+      <Tritone />   
+      <Perfect5th />
       <div>Minor 6th</div>
       <div>Major 6th</div>
       <div>Minor 7th</div>
