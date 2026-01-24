61ffc63a6a94fa007933c6249a1263092a7890ad
Hannah Olbrich <hannaholbrich@gmail.com>
2026-01-24 22:53:03 +0000

    Center the text on the arpeggios page

--- music-theory-project/src/pages/arpeggios/Arpeggios_7ths.jsx
+++ music-theory-project/src/pages/arpeggios/Arpeggios_7ths.jsx
@@ -1,24 +1,25 @@
 import Arpeggios from '../../components/arpeggios/Arpeggios.jsx';
 import Dom7th from '../../components/arpeggios/Dom7th.jsx';
 import Dim7th from '../../components/arpeggios/Dim7th.jsx';
+import './Arpeggios_7ths.css';
 
 export default function Arpeggios_7ths() {
     return (
-      <div>
+      <div className="arpeggios">
         <h1>Arpeggios and 7th Chords</h1>
         <div className="arpeggio">
         <h2>Arpeggios</h2>
-        <Arpeggios />
+        <div className="arpeggio-notation"><Arpeggios /></div>
         <p>An arpeggio is made up of the 1st, 3rd and 5th elements of the scale. </p>
         </div>
         <div className="arpeggio">
         <h2>Dominant 7ths</h2>
-        <Dom7th />      
+        <div className="arpeggio-notation"><Dom7th /></div>   
         <p> A dominant seventh is similar to a normal arpeggio, except an extra 7th note is added in.</p>
         </div>
         <div className="arpeggio">
         <h2>Diminished 7ths</h2>
-        <Dim7th />
+        <div className="arpeggio-notation"><Dim7th /></div>
         <p> A diminished seventh chord is made up of a root note, a minor third, a diminished fifth, and a diminished seventh.</p>
         </div>
       </div>
