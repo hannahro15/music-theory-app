ebb501dcb2576d51d759307e5c08d10e6b959d9b
Hannah Olbrich <hannaholbrich@gmail.com>
2026-01-25 21:20:39 +0000

    Add containers to the intervals

--- music-theory-project/src/pages/intervals/Intervals.jsx
+++ music-theory-project/src/pages/intervals/Intervals.jsx
@@ -10,39 +10,64 @@ import Maj6th from '../../components/intervals/Maj6th.jsx';
 import Min7th from '../../components/intervals/Min7th.jsx';
 import Maj7th from '../../components/intervals/Maj7th.jsx';
 import Octave from '../../components/intervals/Octave.jsx';
+import './intervals.css';
 
 export default function Intervals() {
   return (
     <div>
       <h1>Intervals Page</h1>
       <p>This is the Intervals page showing all the different intervals.</p>
+      <div className="interval-div">
       <h2>Minor 2nd</h2>
       <Min2nd />
       <p>This interval is semitone apart (neighbouring notes).</p>
+      </div>
+      <div className="interval-div">
       <h2>Major 2nd</h2>
       <Maj2nd />
       <p>This interval is two semitones apart (or a tone). </p>
+      </div>
+      <div className="interval-div">
       <h2>Minor 3rd</h2>
       <Min3rd />
+      </div>
+      <div className="interval-div">
       <h2>Major 3rd</h2>
       <Maj3rd />
+      </div>
+      <div className="interval-div">
       <h2>Perfect 4th</h2>
       <Perfect4th />
+      </div>
+      <div className="interval-div">
       <h2>Tritone (or augmented 4th)</h2>
       <Tritone />
       <p>A tritone is also known as an diminished 5th but then the interval would be from a C to a Gb. (It still sounds the same, but different naming conventions).</p>
+      </div>
+      <div className="interval-div">
       <h2>Perfect 5th</h2>
       <Perfect5th />
+      </div>
+      <div className="interval-div">
       <h2>Minor 6th</h2>
       <Min6th />
+      </div>
+      <div className="interval-div">
       <h2>Major 6th</h2>
       <Maj6th />
+      </div>  
+      <div className="interval-div">
       <h2>Minor 7th</h2>
       <Min7th />
+      </div>
+      <div className="interval-div">
       <h2>Major 7th</h2>
       <Maj7th />
+      </div>
+      <div className="interval-div">
       <h2>Octave</h2>
       <Octave />  
+      </div>
     </div>
   );
 };
