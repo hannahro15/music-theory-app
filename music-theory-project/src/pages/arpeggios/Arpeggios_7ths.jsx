04cde4db0bfd54c7c5ac074babb8702821129f49
Hannah Olbrich <hannaholbrich@gmail.com>
2026-01-26 20:58:37 +0000

    Add borders to the arpeggios

--- music-theory-project/src/pages/arpeggios/Arpeggios_7ths.jsx
+++ music-theory-project/src/pages/arpeggios/Arpeggios_7ths.jsx
@@ -9,21 +9,27 @@ export default function Arpeggios_7ths() {
       <div className="arpeggios">
         <h1>Arpeggios and 7th Chords</h1>
         <h2>Arpeggios</h2>
-        <h3>Major Arpeggio</h3>
-        <div className="maj-arpeggio"><MajArpeggio />
-        <p>An arpeggio is made up of the 1st, 3rd and 5th elements of the scale. This is a C major Arpeggio.</p>
-        </div>
-        <h3>Minor Arpeggio</h3>
-        <div className="min-arpeggio"><MinArpeggio />
-        <p>This is a C minor Arpeggio. The 3rd note is flattened, the interval between the 1st and 3rd notes is a minor 3rd.</p>
-        </div>
-        <h2>Dominant 7ths</h2>
-        <div className="arpeggio-notation"><Dom7th />  
-        <p> A dominant seventh is similar to a normal arpeggio, except an extra 7th note is added in.</p>
-        </div>
-        <h2>Diminished 7ths</h2>
-        <div className="arpeggio-notation"><Dim7th />
-        <p> A diminished seventh chord is made up of a root note, a minor third, a diminished fifth, and a diminished seventh.</p>
+        <div className="arpeggio-notation">
+          <div className="arpeggio-border">
+            <h3>Major Arpeggio</h3>
+            <MajArpeggio />
+            <p>An arpeggio is made up of the 1st, 3rd and 5th elements of the scale. This is a C major Arpeggio.</p>
+          </div>
+          <div className="arpeggio-border">
+            <h3>Minor Arpeggio</h3>
+            <MinArpeggio />
+            <p>This is a C minor Arpeggio. The 3rd note is flattened, the interval between the 1st and 3rd notes is a minor 3rd.</p>
+          </div>
+          <div className="arpeggio-border">
+            <h2>Dominant 7ths</h2>
+            <Dom7th />  
+            <p> A dominant seventh is similar to a normal arpeggio, except an extra 7th note is added in.</p>
+          </div>
+          <div className="arpeggio-border">
+            <h2>Diminished 7ths</h2>
+            <Dim7th />
+            <p> A diminished seventh chord is made up of a root note, a minor third, a diminished fifth, and a diminished seventh.</p>
+          </div>
         </div>
       </div>
     );
