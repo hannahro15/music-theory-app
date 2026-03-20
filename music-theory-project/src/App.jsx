5eef45c14e4e978270db7a26a1e70ef499708cc1
Hannah Olbrich <hannaholbrich@gmail.com>
2026-03-20 22:39:06 +0000

    Add and fix all the routes and navigation

--- music-theory-project/src/App.jsx
+++ music-theory-project/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/layout/Navbar.jsx'
 import { Routes, Route } from 'react-router-dom'
-import Scales from './theory/scales/scales.jsx'
+import Scales from './theory/scales/Scales.jsx'
 import Home from './pages/home/Home.jsx'
 import Intervals from './theory/intervals/Intervals.jsx'
 import Arpeggios7ths from './theory/arpeggios/Arpeggios7ths.jsx'
@@ -15,9 +15,20 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/theory/scales" element={<Scales />} />
         <Route path="/theory/arpeggios" element={<Arpeggios7ths />} />
+        <Route path="/theory/circle-of-fifths" element={<h1>Circle of Fifths</h1>} />
+        <Route path="/theory/key-signatures" element={<h1>Key Signatures</h1>} />
         <Route path="/theory/intervals" element={<Intervals />} />
         <Route path="/theory/musical-glossary" element={<MusicalGlossary />} />
         <Route path="/activities/ear-training" element={<h1>Ear Training</h1>} />
+        <Route path="/activities/notation" element={<h1>Notation game</h1>} />
+        <Route path="/activities/intervals" element={<h1>Intervals Quiz</h1>} />
+        <Route path="/activities/chords" element={<h1>Chords Quiz</h1>} />
+        <Route path="/activities/rhythm" element={<h1>Rhythm Game</h1>} />
+        <Route path="/activities/scales" element={<h1>Scales Flashcards Game</h1>} />
+        <Route path="/activities/classical-music-quiz" element={<h1>Classical Music Quiz</h1>} />
+        <Route path="/instrument/violin/fingerings" element={<h1>Violin Fingerings</h1>} />
+        <Route path="/instrument/violin/technique" element={<h1>Violin Technique</h1>} />
+        <Route path="/instrument/violin/excerpts" element={<h1>Violin Excerpts</h1>} />
       </Routes>
     </div>
   )
--- music-theory-project/src/components/layout/Navbar.jsx
+++ music-theory-project/src/components/layout/Navbar.jsx
@@ -16,29 +16,29 @@ export default function Navbar() {
             <NavDropdown title="Theory" id="theory-dropdown">
               <NavDropdown.Item as={NavLink} to="/theory/scales">Scales</NavDropdown.Item>
               <NavDropdown.Item as={NavLink} to="/theory/arpeggios">Arpeggios and 7th Chords</NavDropdown.Item>
-              <NavDropdown.Item href="#circle-of-fifths">Circle of Fifths</NavDropdown.Item>
-              <NavDropdown.Item href="#key-signatures">Key Signatures</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/theory/circle-of-fifths">Circle of Fifths</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/theory/key-signatures">Key Signatures</NavDropdown.Item>
               <NavDropdown.Item  as={NavLink} to="/theory/intervals">Intervals</NavDropdown.Item>
               <NavDropdown.Item as={NavLink} to="/theory/musical-glossary">Musical Glossary</NavDropdown.Item>
             </NavDropdown>
                 
             <NavDropdown title="Activities" id="quizzes-dropdown">
               <NavDropdown.Item as={NavLink} to="/activities/ear-training">Ear training</NavDropdown.Item>
-              <NavDropdown.Item href="#notation">Notation</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/activities/notation">Notation</NavDropdown.Item>
               <NavDropdown.Item as={NavLink} to="/activities/intervals">Intervals</NavDropdown.Item>
-              <NavDropdown.Item href="#chords">Chords</NavDropdown.Item>
-              <NavDropdown.Item href="#rhythm">Rhythm</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/activities/chords">Chords</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/activities/rhythm">Rhythm</NavDropdown.Item>
               <NavDropdown.Item as={NavLink} to="/activities/scales">Scales</NavDropdown.Item>
-              <NavDropdown.Item href="#scales-quiz">Classical Music Quiz</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/activities/classical-music-quiz">Classical Music Quiz</NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Instrument" id="instrument-dropdown">
               <Dropdown drop="end" as="div" className="dropdown-submenu">
                 <Dropdown.Toggle as="a" className="dropdown-item">Violin</Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item href="#violin-fingerings">Fingerings</Dropdown.Item>
-                  <Dropdown.Item href="#violin-technique">Technique</Dropdown.Item>
-                  <Dropdown.Item href="#violin-excerpts">Orchestral Excerpts</Dropdown.Item>
+                  <Dropdown.Item as={NavLink} to="/instrument/violin/fingerings">Fingerings</Dropdown.Item>
+                  <Dropdown.Item as={NavLink} to="/instrument/violin/technique">Technique</Dropdown.Item>
+                  <Dropdown.Item as={NavLink} to="/instrument/violin/excerpts">Orchestral Excerpts</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
             </NavDropdown>
