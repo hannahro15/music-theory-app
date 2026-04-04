014c85dd7e3b3d6a43f0e882a459700300195611
Hannah Olbrich <hannaholbrich@gmail.com>
2026-04-04 10:33:32 +0100

    Fix: add missing Quiz import and HashRouter for GitHub Pages

--- music-theory-project/src/App.jsx
+++ music-theory-project/src/App.jsx
@@ -5,7 +5,8 @@ import Scales from './theory/scales/Scales.jsx'
 import Home from './pages/home/Home.jsx'
 import Intervals from './theory/intervals/Intervals.jsx'
 import Arpeggios7ths from './theory/arpeggios/Arpeggios7ths.jsx'
-import MusicalGlossary from './theory/MusicalGlossary.jsx'
+
+import Quiz from './activities/quiz/Quiz.jsx'
 
 export default function App() {
   return (
